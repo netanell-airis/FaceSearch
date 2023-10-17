3af98980b4e9713e20e4c44cdfe6477d29b16e85
anna-airis <anna@airis-labs.com>
2023-10-17 13:32:02 +0300

    edit my pipeline script

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -3,7 +3,7 @@
 root_dir=/home/anna-airis/work/data/FaceSearch/test_video
 
 python scripts/video_to_frames.py --input_directory ${root_dir}
-#python frames_to_faces.py --input_directory ${root_dir}
+python scripts/frames_to_faces.py --input_directory ${root_dir}
 #python faces_to_embeddings.py --input_directory ${root_dir}
 #python face_to_faceid.py --input_directory ${root_dir} --config_deepsort ../configs/deep_sort.yaml 
 #python index_builder --input_directory ${root_dir} --output_directory ${root_dir}.dataset --action build_index
